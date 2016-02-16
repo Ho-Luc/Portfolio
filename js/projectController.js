0f0f2c96e84ea90e51622927e4e4362374d9e605
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-15 16:04:44 -0800

    fixed problem with repeated appends on projects

--- js/projectController.js
+++ js/projectController.js
@@ -2,6 +2,7 @@
   var projectController = {};
 
   projectController.index = function() {
+    $('#project-articles').empty();
     GenerateProjects.fetchAll(projectView.initIndexPage);
     $('main > section').hide();
     $('#project-articles').show();
