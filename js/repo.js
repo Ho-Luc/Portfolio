801a40dc23746215dd995f0b8f2945a484daaf8b
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-17 16:43:02 -0800

    css changes, refactored repo.js, index

--- js/repo.js
+++ js/repo.js
@@ -3,15 +3,10 @@
   repo.all = [];
 
   repo.requestRepo = function(next) {
-    $.ajax({
-      url: 'https://api.github.com/users/Ho-Luc/repos',
-      type: 'GET',
-      headers: {'Authorization':'token ' + githubToken},
-      success: function(data, message, xhr) {
-        console.log(data);
-        repo.all = data;
-      }
-    }).done(next);
+    $.get('https://api.github.com/users/Ho-Luc/repos', function(data, message, xhr) {
+      repo.all = data;
+    }
+  ).done(next);
   };
 
   repo.with = function(fork) {
